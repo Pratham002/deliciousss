09719bb8540630e2da98eda19127241d9f6b7b1d
Pratham002 <pratham7838@gmail.com>
2022-11-19 09:51:56 +0530

    add media queries in search input field

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,26 @@
-import React, {useState} from 'react';
-import styled from "styled-components";
-import {FaSearch} from "react-icons/fa";
-import {useNavigate} from "react-router";
+import React, { useState } from "react"
+import styled from "styled-components"
+import { FaSearch } from "react-icons/fa"
+import { useNavigate } from "react-router"
 
 function Search() {
-
-  const [input, setInput] = useState("");
-  const navigate = useNavigate();
+  const [input, setInput] = useState("")
+  const navigate = useNavigate()
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    navigate('/searched/' + input);
+    e.preventDefault()
+    navigate("/searched/" + input)
   }
 
   return (
     <FormStyle onSubmit={submitHandler}>
       <div>
-        <FaSearch></FaSearch>
+        <FaSearch />
         <input
-           onChange={(e) => setInput(e.target.value)}
-           type="text" 
-           placeholder="Search"
-           value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Search"
+          value={input}
         />
       </div>
     </FormStyle>
@@ -31,27 +30,59 @@ function Search() {
 const FormStyle = styled.form`
   margin: 0rem 10rem;
   div {
-    margin-left: 7rem;
     position: relative;
-    width: 70%;
+    display: flex;
+    justify-content: center;
   }
   input {
-    border: none;
-    background: linear-gradient(35deg, #494949, #313131);
-    font-size: 0.9rem;
+    margin: 0 auto;
+    width: 80%;
     color: white;
-    padding: 0.5rem 2.5rem;
     border: none;
-    border-radius: 0.5rem;
     outline: none;
-    width: 100%;
+    font-size: 0.9rem;
+    padding: 0.5rem 2.5rem;
+    border-radius: 0.5rem;
+    background: linear-gradient(35deg, #494949, #313131);
   }
   svg {
     position: absolute;
     top: 50%;
-    left: 0%;
+    left: 10%;
     transform: translate(100%, -50%);
     color: white;
+  }
+
+  @media (max-width: 991px) {
+    padding: 0;
+    margin: 0;
+    div {
+      margin: 0;
+      width: 100%;
+    }
+    input {
+      width: 60%;
+      margin: 0 auto;
+    }
+    svg {
+      left: 20%;
+    }
+  }
+  @media (max-width: 767px) {
+    input {
+      width: 80%;
+    }
+    svg {
+      left: 10%;
+    }
+  }
+  @media (max-width: 575px) {
+    input {
+      width: 95%;
+    }
+    svg {
+      left: 2%;
+    }
   }
 `
 
